107c120c9ef7247bf6de420a908bec554a036d8f
annabelle <annabelld@baptisthealth.net>
2019-10-19 16:14:01 -0400

    first commit

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  link: {
+    type: String,
+    required: true
+  },
+
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+Articleschema.index({title: "text"});
+
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+
+module.exports = Article;
